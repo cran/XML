3f4e63ff6062d8f9663fe4892078eca4fbb417e9
Ivan Krylov <ikrylov@disroot.org>
2026-08-21 22:00:58 +0000

    version 3.99-0.24

---
+++ R/defunct.R
@@ -0,0 +1,11 @@
+dtdIsAttribute <- function(name, element, dtd) .Defunct(package = 'XML')
+dtdValidElement <- function(name, within, dtd, pos=NULL) .Defunct(package = 'XML')
+dtdElementValidEntry <- function(element, name, pos=NULL) .Defunct(package = 'XML')
+xmlContainsEntity <- function(name, dtd) .Defunct(package = 'XML')
+xmlContainsElement <- function(name, dtd) .Defunct(package = 'XML')
+dtdEntity <- function(name, dtd) .Defunct(package = 'XML')
+dtdElement <- function(name, dtd) .Defunct(package = 'XML')
+parseDTD <- function(
+	extId, asText = FALSE, name = "", isURL = FALSE, error = xmlErrorCumulator()
+) .Defunct(package = 'XML')
+
--- R/parseDTD.R
+++
@@ -1,6 +0,0 @@
-parseDTD <- 
-function(extId, asText = FALSE, name = "", isURL = FALSE, error = xmlErrorCumulator())
-{
-  .Deprecated(msg = "'parseDTD' no longer works, sorry.", package = 'XML')
-  structure(list(), class = c('InternalDTD', 'ExternalDTD'))
-}
